f7bbf368a08a9817b60721dd864786af882e06b9
Dominik Spicher <dominikspicher@gmail.com>
2020-09-11 10:05:00 +0200

    xoshiro: use From<> constructors

--- src/xoshiro.rs
+++ src/xoshiro.rs
@@ -29,43 +29,45 @@ impl Xoshiro256 {
     pub fn next_int(&mut self, low: u64, high: u64) -> u64 {
         (self.next_double() * ((high - low + 1) as f64)) as u64 + low
     }
-}
 
-#[must_use]
-pub fn from_str(s: &str) -> Xoshiro256 {
-    let mut hasher = crypto::sha2::Sha256::new();
-    hasher.input_str(s);
-    let mut res = [0_u8; 32];
-    hasher.result(&mut res);
-    from_slice(res)
-}
-
-#[must_use]
-pub fn from_crc(bytes: &[u8]) -> Xoshiro256 {
-    let checksum = crc::crc32::checksum_ieee(bytes);
-    let mut hasher = crypto::sha2::Sha256::new();
-    hasher.input(&checksum.to_be_bytes());
-    let mut res = [0_u8; 32];
-    hasher.result(&mut res);
-    from_slice(res)
-}
-
-#[must_use]
-pub fn from_slice(bytes: [u8; 32]) -> Xoshiro256 {
-    let mut s = [0_u8; 32];
-    for i in 0..4 {
-        let o = i * 8;
-        let mut v: u64 = 0;
-        for n in 0..8 {
-            v <<= 8;
-            v |= u64::from(bytes[o + n]);
-        }
-        let bytes = v.to_le_bytes();
-        for n in 0..8 {
-            s[8 * i + n] = bytes[n];
-        }
+    #[must_use]
+    pub fn from_crc(bytes: &[u8]) -> Self {
+        let checksum = crc::crc32::checksum_ieee(bytes);
+        let mut hasher = crypto::sha2::Sha256::new();
+        hasher.input(&checksum.to_be_bytes());
+        let mut res = [0_u8; 32];
+        hasher.result(&mut res);
+        Self::from(res)
     }
-    Xoshiro256StarStar::from_seed(s).into()
+}
+
+impl From<&str> for Xoshiro256 {
+    fn from(value: &str) -> Self {
+        let mut hasher = crypto::sha2::Sha256::new();
+        hasher.input_str(value);
+        let mut res = [0_u8; 32];
+        hasher.result(&mut res);
+        Self::from(res)
+    }
+}
+
+impl From<[u8; 32]> for Xoshiro256 {
+    fn from(value: [u8; 32]) -> Self {
+        let mut s = [0_u8; 32];
+        for i in 0..4 {
+            let o = i * 8;
+            let mut v: u64 = 0;
+            for n in 0..8 {
+                v <<= 8;
+                v |= u64::from(value[o + n]);
+            }
+            let bytes = v.to_le_bytes();
+            for n in 0..8 {
+                s[8 * i + n] = bytes[n];
+            }
+        }
+        Xoshiro256StarStar::from_seed(s).into()
+    }
 }
 
 #[cfg(test)]
@@ -74,7 +76,7 @@ mod tests {
 
     #[test]
     fn test_rng_1() {
-        let mut rng = from_str("Wolf");
+        let mut rng = Xoshiro256::from("Wolf");
         let expected = vec![
             42, 81, 85, 8, 82, 84, 76, 73, 70, 88, 2, 74, 40, 48, 77, 54, 88, 7, 5, 88, 37, 25, 82,
             13, 69, 59, 30, 39, 11, 82, 19, 99, 45, 87, 30, 15, 32, 22, 89, 44, 92, 77, 29, 78, 4,
@@ -89,7 +91,7 @@ mod tests {
 
     #[test]
     fn test_rng_2() {
-        let mut rng = from_crc(b"Wolf");
+        let mut rng = Xoshiro256::from_crc(b"Wolf");
         let expected = vec![
             88, 44, 94, 74, 0, 99, 7, 77, 68, 35, 47, 78, 19, 21, 50, 15, 42, 36, 91, 11, 85, 39,
             64, 22, 57, 11, 25, 12, 1, 91, 17, 75, 29, 47, 88, 11, 68, 58, 27, 65, 21, 54, 47, 54,
@@ -104,7 +106,7 @@ mod tests {
 
     #[test]
     fn test_rng_3() {
-        let mut rng = from_str("Wolf");
+        let mut rng = Xoshiro256::from("Wolf");
         let expected = vec![
             6, 5, 8, 4, 10, 5, 7, 10, 4, 9, 10, 9, 7, 7, 1, 1, 2, 9, 9, 2, 6, 4, 5, 7, 8, 5, 4, 2,
             3, 8, 7, 4, 5, 1, 10, 9, 3, 10, 2, 6, 8, 5, 7, 9, 3, 1, 5, 2, 7, 1, 4, 4, 4, 4, 9, 4,
