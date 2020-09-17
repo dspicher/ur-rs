ab4ec1196d82b45b918674e84c09832efb3e4fed
Dominik Spicher <dominikspicher@gmail.com>
2020-09-17 23:10:55 +0200

    fountain: add cbor() on fountain part

--- src/fountain.rs
+++ src/fountain.rs
@@ -57,6 +57,23 @@ impl std::fmt::Display for Part {
     }
 }
 
+impl Part {
+    #[must_use]
+    #[allow(clippy::cast_possible_truncation)]
+    pub fn cbor(&self) -> String {
+        let mut e = cbor::Encoder::from_memory();
+        e.encode(vec![cbor::Cbor::Array(vec![
+            cbor::Cbor::Unsigned(cbor::CborUnsigned::UInt32(self.sequence as u32)),
+            cbor::Cbor::Unsigned(cbor::CborUnsigned::UInt32(self.sequence_count as u32)),
+            cbor::Cbor::Unsigned(cbor::CborUnsigned::UInt32(self.message_length as u32)),
+            cbor::Cbor::Unsigned(cbor::CborUnsigned::UInt32(self.checksum)),
+            cbor::Cbor::Bytes(cbor::CborBytes(self.data.clone())),
+        ])])
+        .unwrap();
+        hex::encode(e.as_bytes())
+    }
+}
+
 #[must_use]
 #[allow(clippy::cast_possible_truncation)]
 #[allow(clippy::cast_precision_loss)]
@@ -223,6 +240,37 @@ mod tests {
         ];
         for e in expected_parts {
             assert_eq!(encoder.next_part().to_string(), e);
+        }
+    }
+
+    #[test]
+    fn test_fountain_encoder_cbor() {
+        let message = crate::xoshiro::test_utils::make_message("Wolf", 256);
+        let mut encoder = Encoder::new(&message, 30);
+        let expected_parts = vec![
+            "8501091901001a0167aa07581d916ec65cf77cadf55cd7f9cda1a1030026ddd42e905b77adc36e4f2d3c",
+            "8502091901001a0167aa07581dcba44f7f04f2de44f42d84c374a0e149136f25b01852545961d55f7f7a",
+            "8503091901001a0167aa07581d8cde6d0e2ec43f3b2dcb644a2209e8c9e34af5c4747984a5e873c9cf5f",
+            "8504091901001a0167aa07581d965e25ee29039fdf8ca74f1c769fc07eb7ebaec46e0695aea6cbd60b3e",
+            "8505091901001a0167aa07581dc4bbff1b9ffe8a9e7240129377b9d3711ed38d412fbb4442256f1e6f59",
+            "8506091901001a0167aa07581d5e0fc57fed451fb0a0101fb76b1fb1e1b88cfdfdaa946294a47de8fff1",
+            "8507091901001a0167aa07581d73f021c0e6f65b05c0a494e50791270a0050a73ae69b6725505a2ec8a5",
+            "8508091901001a0167aa07581d791457c9876dd34aadd192a53aa0dc66b556c0c215c7ceb8248b717c22",
+            "8509091901001a0167aa07581d951e65305b56a3706e3e86eb01c803bbf915d80edcd64d4d0000000000",
+            "850a091901001a0167aa07581d330f0f33a05eead4f331df229871bee733b50de71afd2e5a79f196de09",
+            "850b091901001a0167aa07581d3b205ce5e52d8c24a52cffa34c564fa1af3fdffcd349dc4258ee4ee828",
+            "850c091901001a0167aa07581ddd7bf725ea6c16d531b5f03254783803048ca08b87148daacd1cd7a006",
+            "850d091901001a0167aa07581d760be7ad1c6187902bbc04f539b9ee5eb8ea6833222edea36031306c01",
+            "850e091901001a0167aa07581d5bf4031217d2c3254b088fa7553778b5003632f46e21db129416f65b55",
+            "850f091901001a0167aa07581d73f021c0e6f65b05c0a494e50791270a0050a73ae69b6725505a2ec8a5",
+            "8510091901001a0167aa07581db8546ebfe2048541348910267331c643133f828afec9337c318f71b7df",
+            "8511091901001a0167aa07581d23dedeea74e3a0fb052befabefa13e2f80e4315c9dceed4c8630612e64",
+            "8512091901001a0167aa07581dd01a8daee769ce34b6b35d3ca0005302724abddae405bdb419c0a6b208",
+            "8513091901001a0167aa07581d3171c5dc365766eff25ae47c6f10e7de48cfb8474e050e5fe997a6dc24",
+            "8514091901001a0167aa07581de055c2433562184fa71b4be94f262e200f01c6f74c284b0dc6fae6673f",
+        ];
+        for e in expected_parts {
+            assert_eq!(encoder.next_part().cbor(), e);
         }
     }
 }
