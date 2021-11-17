5ee45c75f2ca1fdae04d7e26f3eb0bfec88358e1
Dominik Spicher <dominikspicher@gmail.com>
2021-11-17 14:21:36 +0100

    ur: add root export of encoding and decoding structs

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,9 @@ pub mod sampler;
 pub mod ur;
 pub mod xoshiro;
 
+pub use self::ur::Decoder;
+pub use self::ur::Encoder;
+
 #[must_use]
 pub fn crc32() -> crc::Crc<u32> {
     crc::Crc::<u32>::new(&crc::CRC_32_ISO_HDLC)
