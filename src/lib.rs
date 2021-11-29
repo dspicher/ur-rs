2952e95afc06aa291ed61c459ce31837dc1cdc49
Dominik Spicher <dominikspicher@gmail.com>
2021-11-29 11:12:52 +0100

    doc: update crate documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,42 @@
-//! `ur` is a crate to interact with "Uniform Resource" encodings of binary data.
+//! `ur` is a crate to interact with ["uniform resource"](https://github.com/BlockchainCommons/Research/blob/master/papers/bcr-2020-005-ur.md) encodings of binary data.
 //! The encoding scheme is optimized for transport in URIs and QR codes.
 //!
-//! # Encode binary data
+//! ## Encode binary data
+//! The [`crate::bytewords`] module defines multiple encoding styles. The minimal
+//! style, demonstrated below, encodes each byte into two characters.
 //! ```
-//! use ur::bytewords::{encode, Style};
-//! let encoded = encode("Some binary data".as_bytes(), &Style::Minimal).unwrap();
+//! use ur::bytewords::{decode, encode, Style};
+//! let data = "Some binary data".as_bytes();
+//! let encoded = encode(data, &Style::Minimal).unwrap();
 //! assert_eq!(encoded, "gujljnihcxidinjthsjpkkcxiehsjyhsnsgdmkht");
+//! let decoded = decode(&encoded, &Style::Minimal).unwrap();
+//! assert_eq!(data, decoded);
 //! ```
 //!
-//! # Split up payloads into uniform resource URIs
-//!
-//! This uses the minimal bytewords encoding scheme demonstrated above.
+//! ## Split up payloads into uniform resource URIs
+//! The encoder splits up payloads into chunks and encodes them into URIs.
+//! The payload part of the URI contains additional information necessary for
+//! decoding, as well as a checksum.
 //! ```
-//! let data = String::from("Some binary data").repeat(100);
-//! let mut encoder = ur::Encoder::new(data.as_bytes(), 10, "bytes").unwrap();
+//! let data = String::from("Ten chars!").repeat(10);
+//! let max_length = 5;
+//! let mut encoder = ur::Encoder::new(data.as_bytes(), max_length, "bytes").unwrap();
 //! let part = encoder.next_part().unwrap();
-//! assert_eq!(part, "ur:bytes/1-160/lpadcsnbcfamfzcybkmuldbwgegujljnihcxidinjthsjpmezolsld");
+//! assert_eq!(part, "ur:bytes/1-20/lpadbbcsiecyvdidatkpfeghihjtcxiabdfevlms");
+//! let part = encoder.next_part().unwrap();
+//! assert_eq!(part, "ur:bytes/2-20/lpaobbcsiecyvdidatkpfeishsjpjkclwewffhad");
 //! ```
 //!
-//! # Emit a stream of URs that can be recombined into the payload
-//!
-//! This example is best understood in the context of an animated QR code
-//! transport. The receiver can start to receive at any time, miss arbitrary
-//! transmissions, and still successfully restores the payload.
+//! ## Emit a stream of URs that can be recombined into the payload
+//! Finally, those URIs can be consumed by a decoder to restore the original
+//! payload. The receiver can start to receive at any time and miss arbitrary
+//! transmissions. This is useful for example in the context of an animated
+//! QR code.
 //! ```
-//! use ur::{Decoder, Encoder};
-//! let data = String::from("Some binary data").repeat(100);
-//! let mut encoder = Encoder::new(data.as_bytes(), 10, "bytes").unwrap();
-//! let mut decoder = Decoder::default();
+//! let data = String::from("Ten chars!").repeat(10);
+//! let max_length = 5;
+//! let mut encoder = ur::Encoder::new(data.as_bytes(), max_length, "bytes").unwrap();
+//! let mut decoder = ur::Decoder::default();
 //! while !decoder.complete() {
 //!     let part = encoder.next_part().unwrap();
 //!     // Simulate some communication loss
