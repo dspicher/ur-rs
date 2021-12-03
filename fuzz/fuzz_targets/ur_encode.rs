3abbc9c686eaf42e56a56c35a0edaea0920be573
Dominik Spicher <dominikspicher@gmail.com>
2021-12-03 22:04:29 +0100

    dont merge

--- fuzz/fuzz_targets/ur_encode.rs
+++ fuzz/fuzz_targets/ur_encode.rs
@@ -3,7 +3,7 @@ use honggfuzz::fuzz;
 fn main() {
     loop {
         fuzz!(|data: &[u8]| {
-            let max_length = 1 + *data.get(0).unwrap() as usize;
+            let max_length = *data.get(0).unwrap() as usize;
             let mut encoder = ur::Encoder::new(data, max_length, "bytes").unwrap();
             let mut decoder = ur::Decoder::default();
             for _ in 0..encoder.fragment_count() {
