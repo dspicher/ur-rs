23fe5f738b6d7b0faabe33e1f32d7108c7b1450d
Dominik Spicher <dominikspicher@gmail.com>
2021-12-03 09:44:25 +0100

    ur: introduce fuzz testing
    We introduce two fuzzing targets, testing encoding-decoding-
    roundtrips for `ur` and `bytewords` encodings.
    
    For successful fuzz runs, only the `input` directory will be
    located in `hfuzz_workspace/<fuzz_target>`. If a crash is
    encoutered, more files will be created there. Hence the
    rationale for the check
    
    `test $(ls hfuzz_workspace/<fuzz_target> | wc -l) -eq 1`

---
+++ fuzz/fuzz_targets/ur_encode.rs
@@ -0,0 +1,16 @@
+use honggfuzz::fuzz;
+
+fn main() {
+    loop {
+        fuzz!(|data: &[u8]| {
+            let max_length = 1 + *data.get(0).unwrap() as usize;
+            let mut encoder = ur::Encoder::new(data, max_length, "bytes").unwrap();
+            let mut decoder = ur::Decoder::default();
+            for _ in 0..encoder.fragment_count() {
+                let part = encoder.next_part().unwrap();
+                decoder.receive(&part).unwrap();
+            }
+            assert_eq!(decoder.message().unwrap(), data);
+        });
+    }
+}
